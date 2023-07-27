5f117619c58361ab65e86e46e9a1ef7e10432b60
Sakshi <sakshi35sin@gmail.com>
2023-07-27 22:30:12 +0530

    Updated

--- home/models.py
+++ home/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# Student Schemas
+class Student(models.Model):
+    #id=models.AutoField()--Automatically Added by Django, it is Primary key
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+    address = models.TextField()
+    #Profile_photo = models.ImageField()
+    file = models.FileField()
+    dob = models.DateField()
+
+
+#Product Schemas
+class product(models.Model):
+    pass
+ # product_name = models.CharField()
