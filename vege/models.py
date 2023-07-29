4739121527baf92cd76900622d286211b16ec04d
Sakshi <sakshi35sin@gmail.com>
2023-07-29 23:30:13 +0530

    "update"

--- vege/models.py
+++ vege/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Recepies(models.Model):
     recipie_name= models.CharField(max_length=100)
     recipie_description = models.TextField()
-    recipie_image = models.ImageField(upload_to="recepie")
+    recipie_image = models.ImageField(upload_to="recepies")
--- vege/views.py
+++ vege/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def recepies(request):
+    data= request.POST
+    print(data)
     return render(request,"recepies.html")
