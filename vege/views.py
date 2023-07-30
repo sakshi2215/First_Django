2c58235b702433d8525a83a960354d9942156c69
Sakshi <sakshi35sin@gmail.com>
2023-07-30 14:16:15 +0530

    updated

--- vege/views.py
+++ vege/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
+from .models import*
 
 # Create your views here.
 def recepies(request):
-    data= request.POST
-    print(data)
+    if request.method == "POST":
+        data= request.POST
+        recepie_image= request.FILES.get('recepie_image')
+        recepie_name = data.get('recepie_name')
+        recepie_description = data.get('recepie_description')
+        print(recepie_name)
+        print(recepie_description)
+        print(recepie_image)
+# To save the data 
+        Recepies.objects.create(
+            recipie_image=recepie_image,
+            recipie_name=recepie_name,
+            recipie_description=recepie_description,
+        )
+        return redirect('/recepies/')
+
+    
     return render(request,"recepies.html")
